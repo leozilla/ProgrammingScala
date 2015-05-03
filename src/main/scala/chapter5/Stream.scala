0a3639b861e4305c8d3f8b1e38b2f5b6dd0930ac
David Leonhartsberger <leozilla@gmail.com>
2015-05-03 13:09:31 +0200

    Added Stream

---
+++ src/main/scala/chapter5/Stream.scala
@@ -0,0 +1,34 @@
+package chapter5
+
+import Stream._
+
+trait Stream[+A] {
+  def toList: List[A] = this match {
+    case Empty => Nil
+    case Cons(h, t) => h() :: t().toList
+  }
+
+  def take(n: Int): Stream[A] = this match {
+    case Cons(h, t) if n > 0 => cons(h(), t().take(n - 1))
+    case Empty => this
+  }
+}
+
+case object Empty extends Stream[Nothing]
+case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
+
+object Stream {
+  def cons[A](hd: => A, tl: => Stream[A]): Stream[A] = {
+    lazy val head = hd
+    lazy val tail = tl
+    Cons(() => head, () => tail)
+  }
+
+  def empty[A]: Stream[A] = Empty
+
+  def apply[A](as: A*): Stream[A] =
+    if (as.isEmpty) empty
+    else cons(as.head, apply(as.tail: _*))
+}
+
+
---
+++ src/main/scala/chapter5/Streams.sc
@@ -0,0 +1,3 @@
+import chapter5.Stream._
+Stream.apply(1, 2, 3, 4).toList
+Stream.apply(1, 2, 3, 4).take(2)
