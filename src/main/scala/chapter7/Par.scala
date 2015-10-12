6993e400ef2f4ca657f5d4cca49e5d7ed404e35b
David Leonhartsberger <leozilla@gmail.com>
2015-10-12 20:38:23 +0200

    Extracted type and started with executor service representation

--- src/main/scala/chapter7/Par.scala
+++ src/main/scala/chapter7/Par.scala
@@ -1,13 +1,52 @@
 package chapter7
 
-import java.util.concurrent.ExecutorService
+import java.util.concurrent.{TimeUnit, ExecutorService}
 
-trait Par[A] {
-  def map2[B, C](a: Par[A], b: Par[B]): Par[C]
+trait Callable[A] { def call: A }
+
+trait Future[A] {
+  def get: A
+  def get(timeout: Long, unit: TimeUnit): A
+  def cancel(evenIfRunning: Boolean): Boolean
+  def isDone: Boolean
+  def isCancelled: Boolean
 }
 
-class StrictPar[A](eval: => A) extends Par[A] {
+object Par {
 
+  type Par[A] = ExecutorService => Future[A]
 
-  override def map2[B, C](a: Par[A], b: Par[B]): Par[C] = ???
+  def map2[A, B, C](a: Par[A], b: Par[B]): Par[C] = ???
+
+  def fork[A](a: => Par[A]): Par[A] = (es: ExecutorService) => ParFuture(es, a)
+
+  def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
+
+  def unit[A](a: A): Par[A] = (es: ExecutorService) => UnitFuture(a)
+
+  private case class UnitFuture[A](get: A) extends Future[A] {
+
+    override def isCancelled: Boolean = false
+
+    override def get(timeout: Long, unit: TimeUnit): A = get
+
+    override def cancel(evenIfRunning: Boolean): Boolean = false
+
+    override def isDone: Boolean = true
+  }
+
+  private case class ParFuture[A](es: ExecutorService, lazyA: => Par[A]) extends Future[A] {
+
+    override def get: A = ???
+
+    override def isCancelled: Boolean = ???
+
+    override def get(timeout: Long, unit: TimeUnit): A = ???
+
+    override def cancel(evenIfRunning: Boolean): Boolean = ???
+
+    override def isDone: Boolean = ???
+  }
 }
+
+
---
+++ src/test/scala/chapter7/ParTest.scala
@@ -0,0 +1,8 @@
+package chapter7
+
+import org.scalatest._
+
+class ParTest extends FlatSpec with Matchers {
+
+
+}
