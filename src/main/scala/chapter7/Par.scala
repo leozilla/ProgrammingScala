e79113e59bbed83e0bf6911829d217bbf2137199
David Leonhartsberger <leozilla@gmail.com>
2015-10-12 21:04:40 +0200

    Used scala types

--- src/main/scala/chapter7/Par.scala
+++ src/main/scala/chapter7/Par.scala
@@ -1,7 +1,11 @@
 package chapter7
 
-import java.util.concurrent.{TimeUnit, ExecutorService}
+import java.util.concurrent._
 
+/*
+abstract class ExecutorService {
+  def submit[A](a: Callable[A]): Future[A]
+}
 trait Callable[A] { def call: A }
 
 trait Future[A] {
@@ -10,19 +14,32 @@ trait Future[A] {
   def cancel(evenIfRunning: Boolean): Boolean
   def isDone: Boolean
   def isCancelled: Boolean
-}
+}*/
 
 object Par {
 
   type Par[A] = ExecutorService => Future[A]
 
-  def map2[A, B, C](a: Par[A], b: Par[B]): Par[C] = ???
+  def map[A, B](a: Par[A])(f: A => B): Par[B] =
+    map2(a, unit())((a, _) => f(a))
 
-  def fork[A](a: => Par[A]): Par[A] = (es: ExecutorService) => ParFuture(es, a)
+  def map2[A, B, C](a: Par[A], b: Par[B])(f: (A,B) => C): Par[C] =
+    (es: ExecutorService) => {
+      val aFuture = a(es)
+      val bFuture = b(es)
+      UnitFuture(f(aFuture.get, bFuture.get))
+    }
 
   def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
 
+  def fork[A](a: => Par[A]): Par[A] =
+    es => es.submit(new Callable[A] {
+      def call = a(es).get
+    })
+
   def unit[A](a: A): Par[A] = (es: ExecutorService) => UnitFuture(a)
+
+  def run[A](p: Par[A]): A = ???
 
   private case class UnitFuture[A](get: A) extends Future[A] {
 
@@ -33,19 +50,6 @@ object Par {
     override def cancel(evenIfRunning: Boolean): Boolean = false
 
     override def isDone: Boolean = true
-  }
-
-  private case class ParFuture[A](es: ExecutorService, lazyA: => Par[A]) extends Future[A] {
-
-    override def get: A = ???
-
-    override def isCancelled: Boolean = ???
-
-    override def get(timeout: Long, unit: TimeUnit): A = ???
-
-    override def cancel(evenIfRunning: Boolean): Boolean = ???
-
-    override def isDone: Boolean = ???
   }
 }
 
--- src/test/scala/chapter7/ParTest.scala
+++ src/test/scala/chapter7/ParTest.scala
@@ -1,8 +1,19 @@
 package chapter7
 
+import chapter7.Par.Par
 import org.scalatest._
 
 class ParTest extends FlatSpec with Matchers {
 
+  "Unit" should "get back value immediatly" in {
+    // given
+    val unitPar: Par[Int] = Par.unit(1)
+
+    // when
+    val actual: Int = Par.run(unitPar)
+
+    // then
+    actual should be (1)
+  }
 
 }
